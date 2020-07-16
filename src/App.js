10610d37dac14f737db6e21b571dc1df10c912d4
bambooka <sumkina.ol@gmail.com>
2020-07-16 17:27:16 +0300

    feat: initial application structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='searchForm'>
+        <input/>
+        <div>
+          <input type='checkbox'/>
+          <input type='checkbox'/>
+          <input type='checkbox'/>
+        </div>
+      </div>
+      <div className='result'>
+        <div>repo1</div>
+        <div>repo2</div>
+        <div>repo3</div>
+      </div>
     </div>
   );
 }
